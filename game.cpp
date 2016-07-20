c39d60b5ec5859c4bdef942bc10a05ded060244c
Magno Gouveia <magno.gouveia@metasix.com.br>
2016-07-20 16:34:22 -0300

    Refactored code, now while uses functions

--- game.cpp
+++ game.cpp
@@ -1,30 +1,8 @@
 #include <iostream>
 #define N 3
-int main(int argc, char **argv){
-	int velha[N][N] = {0};
-	int jogadorDaVez = 1;
-	bool acabou = false;
 
-	do {
-		int linha, coluna;
-		int tentativas = 0;
-		for(int i =0; i < N; i++){
-			for(int j = 0; j < N; j++ ){
-				std::cout<<velha[i][j] << " ";
-			}
-			std::cout << std::endl;
-		}
-		do{
-			
-			std::cout<<"Jogador da vez:"<< jogadorDaVez<<" Digite uma linha e uma coluna"<<(tentativas++ > 0 ? "Tente de 1 a 3" : " " )<< std::endl;
-			std::cin>> linha;
-			std::cin>> coluna;
-		}while((linha < 0 || linha > 3) && (coluna < 0 || coluna > 2) && velha[linha][coluna] != 0);
-		
-		velha[linha][coluna] = jogadorDaVez;
-
-		//verifica linhas
-
+bool verificaCombinacoes( int velha[N][N], int jogadorDaVez,bool acabou ) {
+//verifica linhas
 		int cl;
 	
 		for(int i = 0; i< N; i++){
@@ -68,6 +46,42 @@ int main(int argc, char **argv){
 		if(cd == N)
 			acabou = true;
 
+		return acabou;
+}
+
+int imprimeTabuleiro(int velha[N][N]){
+	for(int i =0; i < N; i++){
+		for(int j = 0; j < N; j++ ){
+			std::cout<<velha[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
+
+int main(int argc, char **argv){
+	int velha[N][N] = {0};
+	int jogadorDaVez = 1;
+	bool acabou = false;
+
+	do {
+		imprimeTabuleiro(velha);
+		int linha, coluna;
+		int tentativas = 0;
+
+		do{
+			
+			std::cout<<"Jogador da vez:"<< jogadorDaVez<<" Digite uma linha e uma coluna"<<(tentativas++ > 0 ? " Tente de 0 a 2" : " " )<< std::endl;
+			std::cin>> linha;
+			std::cin>> coluna;
+		}while(((linha < 0 || linha > 3) || (coluna < 0 || coluna > 2)) ||  velha[linha][coluna] != 0);
+		
+		velha[linha][coluna] = jogadorDaVez;
+
+
+
+		acabou = verificaCombinacoes(velha,jogadorDaVez,acabou);	
+		
 		int total = 0;
 		for(int i = 0; i < N; i++){
 			for(int j = 0; j < N; j++){
@@ -82,7 +96,8 @@ int main(int argc, char **argv){
 
 	
 	}while(!acabou);
-	std::cout<<"Jogo Acabou";
+	std::cout<<"Jogo Acabou"<<std::endl;
+	imprimeTabuleiro(velha);
 
 
 	return 0;
